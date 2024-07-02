0afc50f618df41b68e838dfc88bf2537146307dd
Rajesh Berepalli <rajeshece405441@gmail.com>
2024-07-02 19:53:55 +0530

    test.py is location is updated in gitignore

--- Intro_section/test.py
+++ Intro_section/test.py
@@ -2,3 +2,6 @@ my_dict = dict(key1="lock", key2="value2")
 print(my_dict)
 my_dict2 = {'key1': 'value1', 'key2': 'value2'}
 print(my_dict2)
+my_dict_list = [('key1', 'value1'), ('key2', 'value2')]
+my_dict3 = dict(my_dict_list)
+print(my_dict3)
